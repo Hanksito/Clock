e7e618b4589ce15e6d3b71d4eae0efa93e43428f
Hanksito <barjabalseiro@gmail.com>
2022-07-04 10:59:58 +0000

    Funcional

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,75 @@
-import React from "react";
+import React, {useState, useEffect,useRef} from "react";
+import { render } from "react-dom";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+
+
+const Clock = () => {
+
+const [seconds,setSeconds] = useState(0);
+const [hora,setHora] = useState(0);
+const [minutes,setMinutes] = useState(0);
+const [ampm,setAmpm] = useState("");
+let h = new Date().getHours();
+let m = new Date().getMinutes();
+let s = new Date().getSeconds();
+let am = h >= 12 ? "PM" : "AM";
+
+if(h > 12){
+    h = h - 12;
+}
+h = (h < 10) ? "0" + h : h;
+m = (m < 10) ? "0" + m : m;
+s = (s < 10) ? "0" + s : s;
+
+
+useEffect(() => {
+    const interval = setInterval(() => {
+        setHora(h);
+        setMinutes(m);
+        setSeconds(s);
+        setAmpm(am)
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		
+        <div id="time">
+            <div class="circle" id="red">
+               <div class="dots hr_dot"></div>
+                <svg>
+                    <circle cx="70" cy="70" r="70"></circle>
+                    <circle cx="70" cy="70" r="70" id="hh" ></circle>
+                </svg>
+                <div>{hora}<br/><span>Hora</span></div>
+            </div>
+            <div class="circle" id="green">
+                <div class="dots min_dot"></div>
+                <svg>
+                    <circle cx="70" cy="70" r="70"></circle>
+                    <circle cx="70" cy="70" r="70" id="mm" ></circle>
+                </svg>
+                
+                <div id="minutes">{minutes}<br/><span>minutos</span></div>
+            </div>
+            <div class="circle" id="yellow">
+                <div class="dots sec_dot"></div>
+                <svg>
+                    <circle cx="70" cy="70" r="70"></circle>
+                    <circle cx="70" cy="70" r="70" id="ss" ></circle>
+                </svg>
+                <div id="seconds">{seconds}<br/><span>segundos</span></div>
+            </div>
+            <div class="ap">
+                <div id="ampm">{ampm}</div>
+            </div>
 		</div>
 	);
 };
 
-export default Home;
+
+export default Clock;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,57 @@
 //import react into the bundle
-import React from "react";
+
+import React,{useState} from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Clock from "./component/home.jsx";
+
+
+setInterval(() =>{
+    let hours = document.getElementById("hours");
+    let minutes = document.getElementById("minutes");
+    let seconds = document.getElementById("seconds");
+    let ampm = document.getElementById("ampm");
+    let hh = document.getElementById("hh");
+    let mm = document.getElementById("mm");
+    let ss = document.getElementById("ss")
+    let hr_dot = document.querySelector(".hr_dot");
+    let min_dot = document.querySelector(".min_dot");
+    let sec_dot = document.querySelector(".sec_dot");
+
+    /*let[hora,sethora]=useState();*/
+
+
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let am = h >= 12 ? "PM" : "AM";
+    
+    if(h > 12){
+        h = h - 12;
+    }
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    
+   /* hours.innerHTML = h ;
+    minutes.innerHTML = m 
+    seconds.innerHTML = s 
+    ampm.innerHTML = am;*/
+
+    hh.style.strokeDashoffset = 440 - (440 * h) / 12;
+    mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+    ss.style.strokeDashoffset = 440 - (440 * s) / 60;
+
+    hr_dot.style.transform = `rotate(${h * 30}deg)`;
+    min_dot.style.transform = `rotate(${m * 6}deg)`;
+    sec_dot.style.transform = `rotate(${s * 6}deg)`;
+
+})
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Clock />, document.querySelector("#app"));
